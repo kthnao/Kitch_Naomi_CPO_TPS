5dddd5cabfe16cdbd27a311f5a4de894e09e94d9
naomi <naomi@192.168.115.119>
2021-10-08 08:58:34 +0200

    ajout du TP2

---
+++ TP2_Bieres_Kitch/src/tp2_bieres_kitch/TP2_Bieres_Kitch.java
@@ -0,0 +1,21 @@
+/*
+*nom : KITCH Naomi
+*groupe : TDA
+*rôle du programme : TP2
+ */
+package tp2_bieres_kitch;
+
+/**
+ *
+ * @author naomi
+ */
+public class TP2_Bieres_Kitch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
