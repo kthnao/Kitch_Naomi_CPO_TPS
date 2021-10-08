dd89e8ae2de62861f384af7c37a27e4e93f452c9
naomi <naomi@192.168.115.119>
2021-10-08 09:14:52 +0200

    ajout du TP2

---
+++ TP2_Bieres_Kitch/src/tp2_bieres_kitch/BouteilleBierre.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2_bieres_kitch;
+
+/**
+ *
+ * @author naomi
+ */
+public class BouteilleBierre {
+    String nom;
+    String brasserie;
+    float degreAlcool;
+    boolean ouverte;
+Public void lireEtiquette() {
+
+System.out.println("Bouteille de " + nom +" (" + degreAlcool +
+
+" degres) \nBrasserie : " + brasserie ) ;
+
+}   
+}
