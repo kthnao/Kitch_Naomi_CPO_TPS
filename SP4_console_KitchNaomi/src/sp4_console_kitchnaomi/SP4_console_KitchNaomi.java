228f1fe5beeeb853737c1f5802630ccad62ef9a8
naomi <naomi@192.168.115.119>
2021-10-25 11:04:45 +0200

    ajout du livrable console du projet Super Puissance 4

---
+++ SP4_console_KitchNaomi/src/sp4_console_kitchnaomi/SP4_console_KitchNaomi.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp4_console_kitchnaomi;
+
+/**
+ *
+ * @author naomi
+ */
+public class SP4_console_KitchNaomi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
