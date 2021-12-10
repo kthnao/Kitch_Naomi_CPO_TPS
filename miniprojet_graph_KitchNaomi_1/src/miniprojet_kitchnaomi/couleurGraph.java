05a28b77d8897a5136d54db8d1b2fb90deb74815
naomi <naomi@192.168.115.119>
2021-12-10 11:06:05 +0100

    interface du pannel des couleurs avec les boutons ok

---
+++ miniprojet_graph_KitchNaomi_1/src/miniprojet_kitchnaomi/couleurGraph.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miniprojet_kitchnaomi;
+
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author naomi
+ */
+public class couleurGraph extends JButton {
+
+    String couleurAssociee;
+    ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getResource("/images/blank.gif"));
+    ImageIcon img_rouge = new javax.swing.ImageIcon(getClass().getResource("/images/rouge.gif"));
+    ImageIcon img_fushia = new javax.swing.ImageIcon(getClass().getResource("/images/fuschia.gif"));
+    ImageIcon img_bleu = new javax.swing.ImageIcon(getClass().getResource("/images/bleu.gif"));
+    ImageIcon img_blanc = new javax.swing.ImageIcon(getClass().getResource("/images/blanc.gif"));
+    ImageIcon img_jaune = new javax.swing.ImageIcon(getClass().getResource("/images/jaune.gif"));
+    ImageIcon img_mauve = new javax.swing.ImageIcon(getClass().getResource("/images/mauve.gif"));
+    ImageIcon img_vert = new javax.swing.ImageIcon(getClass().getResource("/images/vert.gif"));
+    ImageIcon img_orange = new javax.swing.ImageIcon(getClass().getResource("/images/orange.gif"));
+
+    public couleurGraph(String couleur) {
+        couleurAssociee = couleur;
+    }
+
+    @Override
+    public void paintComponent(Graphics G) {
+        super.paintComponent(G);
+
+        switch (couleurAssociee) {
+            case "vide":
+                setIcon(img_vide); //on attribue l'image blank.gif
+                break;
+            case "rouge":
+                setIcon(img_rouge);
+                break;
+                case "fuschia":
+                setIcon(img_fushia);
+                break;
+                case "bleu":
+                setIcon(img_bleu);
+                break;
+                case "blanc":
+                setIcon(img_blanc);
+                break;
+                case "jaune":
+                setIcon(img_jaune);
+                break;
+                case "mauve":
+                setIcon(img_mauve);
+                break;
+                case "vert":
+                setIcon(img_vert);
+                break;
+                case "orange":
+                setIcon(img_orange);
+                break;
+             
+        }
+
+    }
+}
